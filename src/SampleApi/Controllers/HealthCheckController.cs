94964af2d789138d3a08f24fcbeb9e04a3a71881
NickLin <nick.lin@allytransport.com>
2025-06-03 18:08:38 +0800

    add basic structure

---
+++ src/SampleApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("readiness")]
+        public IActionResult HealthCheck()
+        {
+            _logger.LogInformation("heathy status");
+            return Ok("Ok");
+        }
+    }
+}
